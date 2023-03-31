754265ae43d38deb8c01312ac09d6759723d3c25
mailmaul <maulmailmaul@gmail.com>
2023-03-31 21:49:37 +0700

    change mainpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:youapp_test/app/ui/pages/addinterest_page/addinterest_page.dart';
+import 'app/ui/pages/loginscreen_page/loginscreen_page.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Inter'),
-      home: AddinterestPage(),
+      home: LoginScreenPage(),
     ),
   );
 }
